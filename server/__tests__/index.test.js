80e2f3c37f37e83e52de6789eb28fe92431a04f1
josephdaw <josephdaw@icloud.com>
2023-04-16 21:26:25 +0930

    mongoose test set up

--- server/__tests__/index.test.js
+++ server/__tests__/index.test.js
@@ -1,13 +1,25 @@
 const mongoose = require('mongoose');
-const { removeAllCollections } = require('../testHelperFunctions')
 
 const DB_NAME = 'mern-tdd-testing';
 const CONNECTION_URL = `mongodb://localhost/${DB_NAME}`;
 
+async function removeAllCollections() {
+    const collections = Object.keys(mongoose.connection.collections)
+    for (const collectionName of collections) {
+      const collection = mongoose.connection.collections[collectionName]
+      await collection.deleteMany()
+    }
+  }
 
-// beforeAll(async () => {
-//     await mongoose.connect(CONNECTION_URL)
-// })
+beforeAll(async () => {
+    await mongoose.connect(CONNECTION_URL)
+})
+
+afterAll(async () => {
+    await removeAllCollections(mongoose.connection.collections)
+    await mongoose.connection.close()
+})
+
 describe('Testing the test module', () => {
 
     test('Basic function test', () => {
@@ -17,6 +29,3 @@ describe('Testing the test module', () => {
 
 })
 
-// afterAll(async () => {
-//     await removeAllCollections(mongoose.connection.collections)
-// })
